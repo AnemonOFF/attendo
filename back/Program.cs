c710447b699dddffcba6398ed9a5922d111ee77e
Miarel <larsidova@mail.ru>
2025-10-11 04:13:01 +0500

    style(back): apply whitespace fixes

--- back/Program.cs
+++ back/Program.cs
@@ -1,23 +1,31 @@
+using MediatR;
+using Attendo.Application;
+using Attendo.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+builder.Services.AddPersistence(builder.Configuration);
+
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssemblies(
+        typeof(Attendo.Application.DependencyInjection).Assembly,
+        typeof(AppDbContext).Assembly
+    )
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
