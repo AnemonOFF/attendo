c9e4d41c0fe82bb4dedb571fefb992a75e726694
Miarel <larsidova@mail.ru>
2025-10-25 16:14:18 +0500

    fix(back): Added initial migrations

--- back/Attendo.Application/Groups/Queries/GetGroups/GetGroupsQuery.cs
+++ back/Attendo.Application/Groups/Queries/GetGroups/GetGroupsQuery.cs
@@ -4,9 +4,9 @@ using MediatR;
 
 namespace Attendo.Application.Groups.Queries
 {
-  public class GetGroupsQuery : IRequest<IReadOnlyList<GroupDto>>
-  {
-    public int? Offset { get; set; }
-    public int? Limit { get; set; }
-  }
+    public class GetGroupsQuery : IRequest<IReadOnlyList<GroupDto>>
+    {
+        public int? Offset { get; set; }
+        public int? Limit { get; set; }
+    }
 }
